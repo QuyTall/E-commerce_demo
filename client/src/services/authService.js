216202486b1b521dd96b4b103070f164c7206d14
Le Quy Cao <lequycaopt123@gmail.com>
2025-12-05 00:00:52 +0700

    Fix login response parsing structure

--- client/src/services/authService.js
+++ client/src/services/authService.js
@@ -1,29 +1,38 @@
 import axios from "axios";
 
-// 👇 QUAN TRỌNG: IP Server AWS của bạn
+// 👇 IP SERVER CỦA BẠN
 const API_URL = "http://100.26.182.209:8080/api/auth";
 
-// Đăng ký
-export const registerUser = async (userData) => {
+const registerUser = async (userData) => {
   const response = await axios.post(`${API_URL}/register`, userData);
   return response.data;
 };
 
-// Đăng nhập
-export const loginUser = async (username, password) => {
+const loginUser = async (username, password) => {
   const response = await axios.post(`${API_URL}/login`, {
     username,
     password,
   });
 
-  if (response.data.token) {
-    localStorage.setItem("user", JSON.stringify(response.data));
+  // 👇 SỬA Ở ĐÂY: Dựa trên JSON bạn gửi {"success": true, "data": {...}}
+  // Chúng ta cần lấy cái cục "data" bên trong
+  const responseData = response.data; 
+  
+  if (responseData.success && responseData.data) {
+      // Lưu đúng cái object chứa token (id, token, username, role)
+      localStorage.setItem("user", JSON.stringify(responseData.data));
+      return responseData.data;
   }
 
-  return response.data; // Trả về data để Redux xử lý tiếp
+  return response.data;
 };
 
-// Đăng xuất
-export const logoutUser = () => {
+const logoutUser = () => {
   localStorage.removeItem("user");
+};
+
+export {
+    registerUser,
+    loginUser,
+    logoutUser
 };
